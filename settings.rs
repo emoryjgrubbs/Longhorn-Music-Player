96881ae1242382ea3a6efaf3f8bade53b35c8a83
Emory Grubbs <emoryjgrubbs@gmail.com>
2025-01-16 19:34:10 -0600

    added more field

--- settings.rs
+++ settings.rs
@@ -1,11 +1,15 @@
 pub struct Reader<'r> {
     max_history: i32,
     library_paths: Vec<&'r str>,
+    top_songs_len: i32,
+    top_albums_len: i32,
+    top_artists_len: i32,
+    top_decay: i32,
 }
 
 impl<'r> Reader<'_> {
     pub fn new() -> Reader<'r> {
-        Reader { max_history: 50, library_paths: vec!["~/Music"] }
+        Reader { max_history: 50, library_paths: vec!["~/Music"], top_songs_len: 100, top_albums_len: 50, top_artists_len: 25, top_decay: 3 }
     }
 
 
@@ -16,6 +20,19 @@ impl<'r> Reader<'_> {
 
     pub fn library_paths(&self) -> Vec<&str> {
         self.library_paths.clone()
+    }
+
+    pub fn top_songs_len(&self) -> i32 {
+        self.top_songs_len.clone()
+    }
+    pub fn top_albums_len(&self) -> i32 {
+        self.top_albums_len.clone()
+    }
+    pub fn top_artists_len(&self) -> i32 {
+        self.top_artists_len.clone()
+    }
+    pub fn top_decay(&self) -> i32 {
+        self.top_decay.clone()
     }
 
 
