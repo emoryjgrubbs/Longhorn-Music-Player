bacc64b474d3dd42a80d976f92ba74fe142c089d
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2025-06-11 19:19:37 -0500

    initial interactivity - ratatui crate providing ui + key event hanlding - current song is displayed at the bottom - play, pause, next/previous song/album, volume up/down controls on keys + key to quit -------------------------------------------------------------------------------------- - something is wrong with the queue, i think something is breaking when the queue is longer than the buffer - jump needs to have the buffer/player functionality added (add when i figure out what's happening with the buffer causing crashes)'

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,38 @@
 #[allow(dead_code)]
 #[allow(unused_parens)]
 
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+use std::{time::Duration};
+
+mod settings;
+use settings::Settings;
+
+mod player;
+use player::Player;
+
+
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
+
+
+
 #[derive(Debug, Clone)]
 struct Song<'a> {
     title: &'a str,
@@ -12,47 +44,125 @@ struct Song<'a> {
 
 #[derive(Debug, Clone)]
 enum Link<'a> {
-    Local(usize),
+    Local(&'a str),
     Plex(&'a str),
     Spotify(&'a str),
     Youtube(&'a str),
 }
 
-mod settings;
-use settings::Settings;
-
-mod player;
-use player::Player;
 
 
-fn main() {
-    let mut user_settings = Settings::new();
-    user_settings.read_config(None, true);
+struct App<'a> {
+    player: Player<'a>,
+    settings: Settings,
 
-    let mut player = Player::new(user_settings.history_len());
+    exit: bool,
+}
+impl<'a> App<'a> {
+    // setup
+    fn default() -> App<'a> {
+        let mut settings = Settings::new();
+        settings.read_config(None, false);
+        let player = Player::new(settings.history_len(), 0.1, 9);
+        App { settings, player, exit: false }
+    }
 
-    player.test_play();
-    /*
-    let song_one = Song { title: "track 1", album: "Album 1", artist: "Artist 1", link: Link::Spotify("Spotify Link 1") };
-    let song_two = Song { title: "track 2", album: "Album 1", artist: "Artist 1", link: Link::Spotify("Spotify Link 2") };
-    let song_three = Song { title: "track 3", album: "Album 2", artist: "Artist 2", link: Link::Spotify("Spotify Link 3") };
-    let song_four = Song { title: "track 4", album: "Album 3", artist: "Artist 3", link: Link::Spotify("Spotify Link 4") };
-    let song_five = Song { title: "track 5", album: "Album 3", artist: "Artist 3", link: Link::Spotify("Spotify Link 5") };
+    // main interactivity loop
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
 
-    let new_songs_one = vec![ song_one, song_two, song_three ];
-    let new_songs_two = vec![ song_four, song_five ];
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
 
-    player.queue_after_album(new_songs_one);
-    player.queue_after_album(new_songs_two);
-    println!("");
-    player.test_print();
+    fn handle_events(&mut self) -> io::Result<()> {
+        if self.player.check_completed() {
+            self.player.advance_song();
+        }
+        // make sure there is an event, so read doesn't just block
+        if let Ok(true) = event::poll(Duration::from_millis(20)) {
+            match event::read()? {
+                // it's important to check that the event is a key press event as
+                // crossterm also emits key release and repeat events on Windows.
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    self.handle_key_event(key_event)
+                }
+                _ => {}
+            };
+        }
+        Ok(())
+    }
 
-    println!("");
-    player.jump_to_song(2);
-    player.test_print();
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => { self.exit = true },
+            // movement keys
+            KeyCode::Char('h') => {},
+            KeyCode::Char('j') => {},
+            KeyCode::Char('k') => {},
+            KeyCode::Char('l') => {},
+            // player controls
+            KeyCode::Char('a') => { self.player.devance_song(); },
+            KeyCode::Char('s') => { self.player.stop() },
+            KeyCode::Char('d') => { self.player.toggle_play() },
+            KeyCode::Char('f') => { self.player.advance_song(); },
+            KeyCode::Char('A') => { self.player.devance_album(); },
+            // TODO this increment should come from user settings
+            KeyCode::Char('S') => { self.player.volume_down(0.01); },
+            // TODO this increment should come from user settings
+            KeyCode::Char('D') => { self.player.volume_up(0.01); },
+            KeyCode::Char('F') => { self.player.advance_album(); },
 
-    println!("");
-    player.jump_to_song(-1);
-    player.test_print();
-    */
+            // TODO REMOVE
+            KeyCode::Char(' ') => {
+                let test_songs = vec![
+                    Song { title: "Why You'd Want to Live Here", album: "The Photo Album", artist: "Death Cab for Cutie", link: Link::Local("./test-track-one.flac") },
+                    Song { title: "Everytime You Touch Me", album: "Disk", artist: "Moby", link: Link::Local("./test-track-two.wav") },
+                    Song { title: "Invisible Face", album: "Nonagon Infinity", artist: "King Gizzard & The Lizard Wizard", link: Link::Local("./test-track-four.flac") },
+                    Song { title: "Wah Wah", album: "Nonagon Infinity", artist: "King Gizzard & The Lizard Wizard", link: Link::Local("./test-track-five.flac") },
+                    Song { title: "The Lost Art of Keeping A Secret", album: "Rated R", artist: "Queens of the Stone Age", link: Link::Local("./test-track-three.mp3") },
+                ];
+
+                self.player.queue_after_current(test_songs);
+            },
+            _ => {}
+        }
+    }
+}
+
+impl Widget for &App<'_> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Longhorn Music Player ".bold());
+        let play_status = {
+            if self.player.check_playing() { "" }
+            else { "(PAUSED) " }
+        };
+        let instructions = {
+            if let Some(song) = &self.player.current_song() {
+                Line::from(vec![
+                    " ".into(),
+                    song.artist.into(),
+                    "/".into(),
+                    song.album.into(),
+                    "/".into(),
+                    song.title.into(),
+                    " ".into(),
+                    play_status.into()
+                ])
+            }
+            else {
+                Line::from(" QUEUE EMPTY ")
+            }
+        };
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions)
+            .border_set(border::THICK)
+            .render(area, buf);
+    }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,6 @@
 use crate::Song;
-use std::{collections::{vec_deque, VecDeque}, vec};
+use crate::Link;
+use std::{collections::{VecDeque}, vec};
 use soloud::*;
 
 #[derive(Debug)]
@@ -11,13 +12,14 @@ pub struct Player<'a> {
     buff_tracks: Vec<soloud::audio::Wav>,
     buff_pos: usize,
     player: Soloud,
+    playing: bool,
 }
 
 #[allow(dead_code)]
 impl<'q> Player<'q> {
-    pub fn new(max_history: i32) -> Player<'q> {
+    pub fn new(max_history: i32, initial_volume: f32, buffer_size: u32) -> Player<'q> {
         let mut new_player = Player { max_history, history_songs: VecDeque::new(), current_song: None, queue_songs: VecDeque::new(),
-            buff_tracks: vec![], buff_pos: 0, player: Soloud::default().expect("failed to open soloud player")
+            buff_tracks: vec![], buff_pos: 0, player: Soloud::default().expect("failed to open soloud player"), playing: false,
         };
 
         // arbitrary number of slots in track buffer
@@ -25,49 +27,22 @@ impl<'q> Player<'q> {
         //  and half being the current track
         //  intention is for buffer to keep enough tracks loaded
         //  for responsive play back, while keeping memory ussage down
-        for _ in 0..3 {
+        for _ in 0..buffer_size {
             new_player.buff_tracks.push(audio::Wav::default());
         }
 
+        new_player.player.set_global_volume(initial_volume);
+
         new_player
-    }
-
-    // TODO remove this
-    pub fn test_play(&mut self) {
-        let test_tracks = vec!["./test-track-four.flac", "./test-track-five.flac", "./test-track-one.flac", "./test-track-two.wav", "./test-track-three.mp3"];
-        for track in test_tracks {
-            print!("playing {} ", track);
-            // TODO have handling incase a track disapears
-            // loads next track to buffer
-            let up_ccoming_pos = (self.buff_pos + 1) % self.buff_tracks.len();
-            if let Err(msg) = self.buff_tracks[up_ccoming_pos].load(&std::path::Path::new(track)) {
-                // TODO handle sl errors properly
-                println!("Encountered error: {}...", msg)
-            }
-
-            // lets current track play
-            //  not fully seamless..
-            while self.player.voice_count() > 0 {
-                std::thread::sleep(std::time::Duration::from_millis(50));
-            }
-            
-            self.buff_pos += 1;
-            self.buff_pos %= self.buff_tracks.len();
-
-            println!("at {} in buff", self.buff_pos);
-            // starts new track playing
-            self.player.play(&self.buff_tracks[self.buff_pos]);
-        }
-
-        // lets last track finish
-        while self.player.voice_count() > 0 {
-            std::thread::sleep(std::time::Duration::from_millis(50));
-        }
     }
 
     pub fn set_max(&mut self, new_max_history: i32) {
         self.max_history = new_max_history;
         self.cull_excess_history();
+    }
+
+    pub fn current_song(&self) -> Option<Song> {
+        self.current_song.clone()
     }
 
     pub fn queue_after_current(&mut self, new_songs: Vec<Song<'q>>) {
@@ -78,6 +53,29 @@ impl<'q> Player<'q> {
 
         if let None = self.current_song {
             self.current_song = self.queue_songs.pop_front();
+            if let Some(song) = &self.current_song {
+                if let Link::Local(path) = song.link {
+                    if let Err(_) = self.buff_tracks[self.buff_pos].load(&std::path::Path::new(path)) { todo!() }
+                    self.player.play(&self.buff_tracks[self.buff_pos]);
+                    if !self.playing {
+                        self.player.set_pause_all(true);
+                    }
+                }
+            }
+        }
+
+        // load possible new songs into buffer
+        for load_offset in 0..(self.buff_tracks.len()/2) {
+            if let Some(song) = self.queue_songs.get(load_offset) {
+                // TODO these should probably become match statements
+                //  when i implement other type of play handling
+                if let Link::Local(path) = song.link {
+                    let load_pos = (self.buff_pos + 1 + load_offset).rem_euclid(self.buff_tracks.len());
+                    if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                }
+            }
+            // if there aren't more songs in the queue, no point in coninuing
+            else { break }
         }
     }
 
@@ -104,15 +102,62 @@ impl<'q> Player<'q> {
 
         if let None = self.current_song {
             self.current_song = self.queue_songs.pop_front();
+            if let Some(song) = &self.current_song {
+                if let Link::Local(path) = song.link {
+                    if let Err(_) = self.buff_tracks[self.buff_pos].load(&std::path::Path::new(path)) { todo!() }
+                    self.player.play(&self.buff_tracks[self.buff_pos]);
+                    if !self.playing {
+                        self.player.set_pause_all(true);
+                    }
+                }
+            }
+        }
+
+        // load possible new songs into buffer
+        for load_offset in splice_loc..(self.buff_tracks.len()/2) {
+            if let Some(song) = self.queue_songs.get(load_offset) {
+                // TODO these should probably become match statements
+                //  when i implement other type of play handling
+                if let Link::Local(path) = song.link {
+                    let load_pos = (self.buff_pos + 1 + load_offset).rem_euclid(self.buff_tracks.len());
+                    if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                }
+            }
+            // if there aren't more songs in the queue, no point in coninuing
+            else { break }
         }
     }
 
     pub fn queue_after_queue(&mut self, new_songs: Vec<Song<'q>>) {
         let mut deque_new_songs = VecDeque::from(new_songs);
+        let old_queue_len = self.queue_songs.len();
         self.queue_songs.append(&mut deque_new_songs);
 
         if let None = self.current_song {
             self.current_song = self.queue_songs.pop_front();
+            if let Some(song) = &self.current_song {
+                if let Link::Local(path) = song.link {
+                    if let Err(_) = self.buff_tracks[self.buff_pos].load(&std::path::Path::new(path)) { todo!() }
+                    self.player.play(&self.buff_tracks[self.buff_pos]);
+                    if !self.playing {
+                        self.player.set_pause_all(true);
+                    }
+                }
+            }
+        }
+
+        // load possible new songs into buffer
+        for load_offset in old_queue_len..(self.buff_tracks.len()/2) {
+            if let Some(song) = self.queue_songs.get(load_offset) {
+                // TODO these should probably become match statements
+                //  when i implement other type of play handling
+                if let Link::Local(path) = song.link {
+                    let load_pos = (self.buff_pos + 1 + load_offset).rem_euclid(self.buff_tracks.len());
+                    if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                }
+            }
+            // if there aren't more songs in the queue, no point in coninuing
+            else { break }
         }
     }
 
@@ -124,19 +169,57 @@ impl<'q> Player<'q> {
             }
             self.current_song = Some(new_song);
             self.cull_excess_history();
+
+            // advance buffer
+            self.player.stop_all();
+            self.buff_pos += 1;
+            self.buff_pos = self.buff_pos.rem_euclid(self.buff_tracks.len());
+            // resume playback
+            self.player.play(&self.buff_tracks[self.buff_pos]);
+            if !self.playing {
+                self.player.set_pause_all(true);
+            }
+            // load new song to buffer
+            let buff_max_future = self.buff_tracks.len() / 2;
+            if let Some(song) = self.queue_songs.get(buff_max_future) {
+                if let Link::Local(path) = song.link {
+                    let load_pos = (self.buff_pos + buff_max_future).rem_euclid(self.buff_tracks.len());
+                    if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                }
+            }
+        }
+        else {
+            self.player.stop_all();
         }
     }
 
     pub fn advance_album(&mut self) {
+        self.player.stop_all();
         loop {
             // prevent advancing to a none song
             if let Some(new_song) = self.queue_songs.pop_front() {
                 if let Some(old_song) = self.current_song.clone() {
-                    // looking for the first instance of the song album changing
+                    // advance buffer
                     self.history_songs.push_back(old_song.clone());
+                    self.buff_pos += 1;
+                    self.buff_pos = self.buff_pos.rem_euclid(self.buff_tracks.len());
+                    // load new song to buffer
+                    let buff_max_future = self.buff_tracks.len() / 2;
+                    if let Some(song) = self.queue_songs.get(buff_max_future) {
+                        if let Link::Local(path) = song.link {
+                            let load_pos = (self.buff_pos + buff_max_future).rem_euclid(self.buff_tracks.len());
+                            if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                        }
+                    }
+                    // looking for the first instance of the song album changing
                     if new_song.album != old_song.album { 
                         self.current_song = Some(new_song);
+                        self.player.play(&self.buff_tracks[self.buff_pos]);
+                        if !self.playing {
+                            self.player.set_pause_all(true);
+                        }
                         self.cull_excess_history();
+
                         break
                     }
                 }
@@ -149,40 +232,83 @@ impl<'q> Player<'q> {
         }
     }
 
-    pub fn deadvance_song(&mut self) {
-        // prevent deadvancing to a none song
+    pub fn devance_song(&mut self) {
+        // prevent devancing to a none song
         if let Some(new_song) = self.history_songs.pop_back() {
             if let Some(old_song) = self.current_song.clone() {
                 self.queue_songs.push_front(old_song);
             }
             self.current_song = Some(new_song);
+
+            // devance buffer
+            self.player.stop_all();
+            self.buff_pos -= 1;
+            self.buff_pos = self.buff_pos.rem_euclid(self.buff_tracks.len());
+            // resume playback
+            self.player.play(&self.buff_tracks[self.buff_pos]);
+            if !self.playing {
+                self.player.set_pause_all(true);
+            }
+            // load new song to buffer
+            let buff_max_past = self.buff_tracks.len() / 2;
+            if let Some(song) = self.queue_songs.get(buff_max_past) {
+                if let Link::Local(path) = song.link {
+                    let load_pos = (self.buff_pos - buff_max_past).rem_euclid(self.buff_tracks.len());
+                    if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                }
+            }
         }
-        // TODO restart play if at breakpoint
     }
 
-    pub fn deadvance_album(&mut self) {
+    pub fn devance_album(&mut self) {
         if let Some(prev_song) = self.history_songs.back() {
+            self.player.stop_all();
             // NOTE get the album of the previous song
-            //  this means deadvancing on any song after the first
+            //  this means devancing on any song after the first
             //  in an album would restart it, but on the first song
             //  it would go to the previous album
-            let deadvance_album = prev_song.album;
+            let devance_album = prev_song.album;
 
             loop {
-                // prevent deadvancing to a none song
+                // prevent devancing to a none song
                 if let Some(new_song) = self.history_songs.pop_back() {
                     if let Some(old_song) = self.current_song.clone() {
-                        if new_song.album != deadvance_album {
+                        if new_song.album != devance_album {
                             self.history_songs.push_back(new_song);
+                            // resume playback
+                            self.player.play(&self.buff_tracks[self.buff_pos]);
+                            if !self.playing {
+                                self.player.set_pause_all(true);
+                            }
                             break
                         }
                         self.queue_songs.push_front(old_song);
                     }
                     self.current_song = Some(new_song);
+                    // devance buffer
+                    self.buff_pos -= 1;
+                    self.buff_pos = self.buff_pos.rem_euclid(self.buff_tracks.len());
+                    // load new song to buffer
+                    let buff_max_past = self.buff_tracks.len() / 2;
+                    if let Some(song) = self.queue_songs.get(buff_max_past) {
+                        if let Link::Local(path) = song.link {
+                            let load_pos = (self.buff_pos - buff_max_past).rem_euclid(self.buff_tracks.len());
+                            if let Err(_) = self.buff_tracks[load_pos].load(&std::path::Path::new(path)) { todo!() }
+                        }
+                    }
                 }
-                else { break }
+                else {
+                    // resume playback
+                    self.player.play(&self.buff_tracks[self.buff_pos]);
+                    if !self.playing {
+                        self.player.set_pause_all(true);
+                    }
+                    break
+                }
             }
         }
+
+        // TODO resume playback
     }
 
     pub fn jump_to_song(&mut self, relative_pos: i32) {
@@ -231,6 +357,21 @@ impl<'q> Player<'q> {
             // TODO restart song?
         }
         */
+
+        // TODO resume playback
+    }
+
+    pub fn check_completed(&self) -> bool {
+        if self.current_song.is_some() {
+            if self.playing && self.player.voice_count() == 0 {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn check_playing(&self) -> bool {
+        self.playing.clone()
     }
 
     fn cull_excess_history(&mut self) {
@@ -242,5 +383,37 @@ impl<'q> Player<'q> {
             vec_history.splice(..overage, []);
             self.history_songs = VecDeque::from(vec_history);
         }
+    }
+
+    // Interactions
+    pub fn toggle_play(&mut self) {
+        self.player.set_pause_all(self.playing);
+        self.playing = !self.playing;
+    }
+    pub fn stop(&mut self) {
+        // TODO idk what i want to do with this
+        self.player.set_pause_all(true);
+        //self.player.stop_all();
+        self.playing = false;
+    }
+
+    pub fn volume_up(&mut self, increment: f32) {
+        let new_volume = {
+            let new_volume = self.player.global_volume() + increment;
+            // NOTE 3 seems to be the highest volume that applies
+            //  (this is just to make ux a bit better)
+            if new_volume > 3.0 { 3.0 }
+            else { new_volume }
+        };
+        self.player.set_global_volume(new_volume);
+    }
+
+    pub fn volume_down(&mut self, increment: f32) {
+        let new_volume = {
+            let new_volume = self.player.global_volume() - increment;
+            if new_volume < 0.0 { 0.0 }
+            else { new_volume }
+        };
+        self.player.set_global_volume(new_volume);
     }
 }
